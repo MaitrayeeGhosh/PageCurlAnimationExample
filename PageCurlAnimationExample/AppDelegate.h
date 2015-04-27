3ef972a3ca83856fb3b346d9276f833e7b562803
MaitrayeeGhosh <maitrayeeghosh89@gmail.com>
2015-04-27 22:12:00 +0530

    Initial Commit

---
+++ PageCurlAnimationExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageCurlAnimationExample
+//
+//  Created by Maitrayee Ghosh on 27/04/15.
+//  Copyright (c) 2015 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PageCurlAnimationExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PageCurlAnimationExample
+//
+//  Created by Maitrayee Ghosh on 27/04/15.
+//  Copyright (c) 2015 Maitrayee Ghosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
